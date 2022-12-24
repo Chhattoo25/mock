76084fd9304e7e7c8e2db53cdb359a02e1792afe
Chhattoo25 <95302680+Chhattoo25@users.noreply.github.com>
2022-12-24 13:43:15 +0530

    done

--- routes/bookmark.js
+++ routes/bookmark.js
@@ -1,13 +1,14 @@
 
 const {Router} = require('express');
 const { getBookmark, postBookmark, delteBookmark } = require("../controllers/bookmarkController");
+const { authentication } = require('../middleware/authentication');
 
 const BookMarkRouter = Router();
 BookMarkRouter.get("/getbookmark",getBookmark );
 
 
-BookMarkRouter.post("/bookmarkpost",postBookmark );
-BookMarkRouter.delete("/bookmarkpost",delteBookmark );
+BookMarkRouter.post("/bookmarkpost",authentication,postBookmark );
+BookMarkRouter.delete("/bookmarkpost",authentication,delteBookmark );
 
 
 module.exports = {BookMarkRouter}
